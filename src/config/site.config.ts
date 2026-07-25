174ce6081819b945fd98c0703324049bdf926ada
sky <hachimura77@proton.me>
2026-07-25 13:20:11 +0300

    fix icon 4.0

--- src/config/site.config.ts
+++ src/config/site.config.ts
@@ -19,7 +19,7 @@ export const PAGES = [
 
 export const PWA = {
   icons: [
-    { src: '/logo.webp', sizes: '512x512', type: 'image/webp', purpose: 'any maskable' },
+    { src: '/icons/icon-512.webp', sizes: '512x512', type: 'image/webp', purpose: 'any maskable' },
   ],
   cache: {
     urls: [
@@ -29,6 +29,7 @@ export const PWA = {
       '/app.js',
       '/images/logo.webp',
       '/images/gr4.webp',
+      '/icons/icon-512.webp'
     ],
     version: 'v1',
   },
