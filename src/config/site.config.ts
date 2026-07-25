8374124fb6fef31504bcc71e3e4790c253b1864a
sky <hachimura77@proton.me>
2026-07-25 12:21:14 +0300

    Fix sw.js

--- src/config/site.config.ts
+++ src/config/site.config.ts
@@ -19,11 +19,20 @@ export const PAGES = [
 
 export const PWA = {
   icons: [
-    { src: '/icons/icon-192.png', sizes: '192x192', type: 'image/png', purpose: 'any maskable' },
-    { src: '/icons/icon-512.png', sizes: '512x512', type: 'image/png', purpose: 'any maskable' },
+    { src: '/icons/icon-192.webp', sizes: '192x192', type: 'image/webp', purpose: 'any maskable' },
+    { src: '/icons/icon-512.webp', sizes: '512x512', type: 'image/webp', purpose: 'any maskable' },
   ],
   cache: {
-    urls: ['/', '/offline', '/styles/global.css', '/app.js'],
+    urls: [
+      '/',
+      '/manifest.json',
+      '/styles/global.css',
+      '/app.js',
+      '/images/logo.webp',
+      '/images/gr4.webp',
+      '/icons/icon-192.webp',
+      '/icons/icon-512.webp'
+    ],
     version: 'v1',
   },
 };
